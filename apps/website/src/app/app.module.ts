0ead8c6e00ad0c45e28824f56394d0f8ce791930
Jefiozie <jefiozie.bosch@gmail.com>
2020-08-14 16:16:32 +0200

    add initial scully setup

--- apps/website/src/app/app.module.ts
+++ apps/website/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ScullyLibModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ scully.website.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./apps/website/src",
+  projectName: "website",
+  outDir: './dist/static',
+  routes: {
+  }
+};
