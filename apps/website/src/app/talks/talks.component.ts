923fce0a97d66f4805511ed59c88e3c63ebce503
Jefiozie <jefiozie.bosch@gmail.com>
2020-09-01 07:54:34 +0200

    split events

--- apps/website/src/app/talks/talks.component.ts
+++ apps/website/src/app/talks/talks.component.ts
@@ -3,18 +3,26 @@ import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
 import { Observable } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 const regex = RegExp('/talks/[0-9]*');
-
+const today = new Date();
 @Component({
   selector: 'dutch-angular-group-talks',
   templateUrl: './talks.component.html',
   styleUrls: ['./talks.component.css'],
 })
 export class TalksComponent implements OnInit {
-  routes$: Observable<ScullyRoute[]>;
+  routesFuture: ScullyRoute[];
+  routesPast: ScullyRoute[];
   constructor(public scully: ScullyRoutesService) {
-    this.routes$ = this.scully.allRoutes$.pipe(
-      map((routes) => routes.filter((route) => regex.test(route.route)))
-    );
+    this.scully.allRoutes$
+      .pipe(map((routes) => routes.filter((route) => regex.test(route.route))))
+      .subscribe((routes) => {
+        this.routesPast = routes.filter(
+          (route) => new Date(route.date) < today
+        );
+        this.routesFuture = routes.filter(
+          (route) => new Date(route.date) > today
+        );
+      });
   }
 
   ngOnInit(): void {}
