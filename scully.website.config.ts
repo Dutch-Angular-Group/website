6e0a5ec718d8ffca6f6d6fb8bcbde5e3fff653bc
Jefiozie <jefiozie.bosch@gmail.com>
2020-10-26 10:27:39 +0100

    feat: add small optimization plugins

--- scully.website.config.ts
+++ scully.website.config.ts
@@ -1,12 +1,30 @@
 import '@dutchangulargroup/scully-plugin-meetup';
 import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { criticalCSS } from '@scullyio/scully-plugin-critical-css';
+import { MinifyHtml } from 'scully-plugin-minify-html';
 
+const defaultPostRenderers = [criticalCSS, 'seoHrefOptimise', MinifyHtml];
 
+// Configurations
+
+// Markdown
 setPluginConfig('md', {});
+
+//MinifyHtml
+const minifyHtmlOptions = {
+  minifyOptions: {
+    removeComments: false,
+  },
+};
+
+setPluginConfig(MinifyHtml, 'render', minifyHtmlOptions);
+
+//ScullyConfig
 export const config: ScullyConfig = {
   projectRoot: './apps/website/src',
   projectName: 'website',
   outDir: './dist/static',
+  defaultPostRenderers,
   routes: {
     '/talks/:talkid': {
       type: 'meetup',
